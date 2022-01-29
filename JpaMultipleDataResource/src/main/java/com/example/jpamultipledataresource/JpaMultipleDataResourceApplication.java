cb7656a02d9edac1195e82ccb191d5f33ca6b8a1
AndreasCh <andreaschextra1@gmail.com>
2022-01-30 00:21:59 +0200

    Jpa learning

---
+++ JpaMultipleDataResource/src/main/java/com/example/jpamultipledataresource/JpaMultipleDataResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamultipledataresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMultipleDataResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMultipleDataResourceApplication.class, args);
+    }
+
+}
